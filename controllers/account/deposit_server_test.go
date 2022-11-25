8de51c2f1f10b49cbb3480296b3a85ad40869bd9
Muhammad Saleh Solahudin <saleh.solahudin@koinworks.com>
2022-11-26 02:08:24 +0700

    file testing

---
+++ controllers/account/deposit_server_test.go
@@ -0,0 +1,154 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/test/bufconn"
+
+	pb "github.com/MSalehKoinWorks/grpc-two/proto/account"
+)
+
+func dialer() func(context.Context, string) (net.Conn, error) {
+	listener := bufconn.Listen(1024 * 1024)
+	server := grpc.NewServer()
+
+	pb.RegisterDepositServiceServer(server, &DepositServer{})
+
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return func(context.Context, string) (net.Conn, error) {
+		return listener.Dial()
+	}
+}
+
+func TestDepositServer_Deposit(t *testing.T) {
+	tests := []struct {
+		name    string
+		amount  float32
+		res     *pb.Res
+		errCode codes.Code
+		errMsg  string
+	}{
+		{
+			"invalid request with negative amount",
+			-100000.00,
+			nil,
+			codes.InvalidArgument,
+			fmt.Sprintf("cannot deposit %v", -100000.00),
+		},
+		{
+			"valid request with non negative amount",
+			100000.00,
+			&pb.Res{Ok: true},
+			codes.OK,
+			"",
+		},
+	}
+
+	ctx := context.Background()
+
+	conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(dialer()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewDepositServiceClient(conn)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := &pb.Req{Amount: tt.amount}
+
+			response, err := client.Deposit(ctx, request)
+
+			if response != nil {
+				if response.GetOk() != tt.res.GetOk() {
+					t.Error("response: expected", tt.res.GetOk(), "received", response.GetOk())
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", codes.InvalidArgument, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestDepositServer_Withdraw(t *testing.T) {
+	tests := []struct {
+		name    string
+		amount  float32
+		res     *pb.Res
+		errCode codes.Code
+		errMsg  string
+	}{
+		{
+			"invalid request with negative amount",
+			-100000.00,
+			nil,
+			codes.InvalidArgument,
+			fmt.Sprintf("cannot withdraw %v", -100000.00),
+		},
+		{
+			"valid request with non negative amount",
+			1000.00,
+			&pb.Res{Ok: true},
+			codes.OK,
+			"",
+		},
+	}
+
+	ctx := context.Background()
+
+	conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithContextDialer(dialer()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewDepositServiceClient(conn)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := &pb.Req{Amount: tt.amount}
+
+			response, err := client.Withdraw(ctx, request)
+
+			if response != nil {
+				if response.GetOk() != tt.res.GetOk() {
+					t.Error("response: expected", tt.res.GetOk(), "received", response.GetOk())
+				}
+			}
+
+			if err != nil {
+				if er, ok := status.FromError(err); ok {
+					if er.Code() != tt.errCode {
+						t.Error("error code: expected", codes.InvalidArgument, "received", er.Code())
+					}
+					if er.Message() != tt.errMsg {
+						t.Error("error message: expected", tt.errMsg, "received", er.Message())
+					}
+				}
+			}
+		})
+	}
+}
